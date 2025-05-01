c6eb15d3499fee9876c32c3ebc6aa2eb2c270683
Badr Ramoul <badr.ramoul@gmail.com>
2025-05-01 19:07:31 +0200

    Fixing Attempt to read property "id" on null error

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -11,24 +11,31 @@ class ServiceResource extends JsonResource
     {
         return [
             'id'           => $this->id,
-            'name'         => $this->getTranslations('name'), 
+            'name'         => $this->getTranslations('name'),
             'description'  => $this->getTranslations('description'),
             'price'        => $this->price,
             'active'       => (bool) $this->active,
-            'category'     => [
-                'id'    => $this->category->id,
-                'name'  => $this->category->getTranslations('name'),
-                'icon'  => $this->category->icon,
-                'color' => $this->category->color,
-            ],
-            'provider'     => [
-                'id'             => $this->provider->id,
-                'name'           => $this->provider->getTranslations('name'),
-                'photo'          => $this->provider->photo,
-                'specialization' => $this->provider->specialization,
-                'rating'         => $this->provider->rating,
-            ],
-            'created_at'   => $this->created_at->format('Y-m-d H:i'),
+
+            'category'     => $this->whenLoaded('category', function () {
+                return $this->category ? [
+                    'id'    => $this->category->id,
+                    'name'  => $this->category->getTranslations('name'),
+                    'icon'  => $this->category->icon,
+                    'color' => $this->category->color,
+                ] : null;
+            }),
+
+            'provider'     => $this->whenLoaded('provider', function () {
+                return $this->provider ? [
+                    'id'             => $this->provider->id,
+                    'name'           => $this->provider->getTranslations('name'),
+                    'photo'          => $this->provider->photo,
+                    'specialization' => $this->provider->specialization,
+                    'rating'         => $this->provider->rating,
+                ] : null;
+            }),
+
+            'created_at'   => $this->created_at?->format('Y-m-d H:i'),
             'updated_at'   => $this->updated_at,
         ];
     }
