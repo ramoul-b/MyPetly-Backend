595bc022ad9960abdae94119060929241db466fc
RamoulB <badr.ramoul@gmail.com>
2025-06-26 22:11:41 +0200

    Allow cart item requests

--- app/Http/Requests/UpdateCartItemRequest.php
+++ app/Http/Requests/UpdateCartItemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCartItemRequest extends FormRequest
+class UpdateCartItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,8 +22,8 @@ class StoreCartItemRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'product_id' => 'required|exists:products,id',
-            'quantity' => 'required|integer|min:1',
+            'product_id' => 'sometimes|exists:products,id',
+            'quantity' => 'sometimes|integer|min:1',
         ];
-    }    
+    }
 }
