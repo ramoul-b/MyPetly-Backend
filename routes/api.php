a0ecbcd8ba096b3c7310fcd5365f4f1bcf22afed
RamoulB <badr.ramoul@gmail.com>
2025-06-26 22:10:59 +0200

    Add CartController with CRUD routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\Api\{
     ProductController,
     OrderController,
     OrderItemController,
+    CartController,
     PaymentController,
     StripeWebhookController
 };
@@ -190,6 +191,13 @@ Route::prefix('v1')->group(function () {
             Route::delete('/{id}', [ProviderServiceController::class, 'destroy']);
             Route::get('/by-provider/{provider_id}', [ProviderServiceController::class, 'getByProvider']);
             Route::get('/by-service/{service_id}', [ProviderServiceController::class, 'getByService']);
+        });
+
+        Route::prefix('cart')->group(function () {
+            Route::post('/add', [CartController::class, 'add']);
+            Route::delete('/remove/{item}', [CartController::class, 'remove']);
+            Route::put('/update/{item}', [CartController::class, 'update']);
+            Route::get('/', [CartController::class, 'index']);
         });
         /*
         |--------------------------------------------------------------------------
