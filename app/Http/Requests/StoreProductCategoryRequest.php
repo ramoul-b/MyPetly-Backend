b96594f40a58742aa599423e021e3cfec3adb389
RamoulB <badr.ramoul@gmail.com>
2025-08-05 14:13:16 +0200

    Authorize managing product categories

--- app/Http/Requests/StoreProductCategoryRequest.php
+++ app/Http/Requests/StoreProductCategoryRequest.php
@@ -11,7 +11,7 @@ class StoreProductCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()?->can('manage product categories') ?? false;
     }
 
     /**
--- app/Http/Requests/UpdateProductCategoryRequest.php
+++ app/Http/Requests/UpdateProductCategoryRequest.php
@@ -11,7 +11,7 @@ class UpdateProductCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()?->can('manage product categories') ?? false;
     }
 
     /**
